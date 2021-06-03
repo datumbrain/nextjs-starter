1bb2749020239c5c05d01be690bbdcc34f978574
HafizMBilal009 <bilal@datumbrain.com>
2021-06-03 11:06:00 +0500

    Created Starter Project In Next.js

--- components/layout.js
+++ components/layout.js
@@ -32,8 +32,8 @@ export default function Layout({ children, home }) {
               priority
               src='/images/profile.jpg'
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={130}
+              width={160}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -46,8 +46,8 @@ export default function Layout({ children, home }) {
                   priority
                   src='/images/profile.jpg'
                   className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
+                  height={100}
+                  width={130}
                   alt={name}
                 />
               </a>
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          I am a Full-Stack Software Engineer with hands on experience in
+          ReacrJS, NodeJS, ExpressJS & Next.js
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
